6fb62d195bde71db7a14c275306f40ec28465b44
miyabisai <guirec0417@gmail.com>
2022-12-03 00:19:35 +0900

    add validators middleware

---
+++ backend/validators/auth.js
@@ -0,0 +1,15 @@
+const {check} = require('express-validator');
+
+exports.userSignupValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+    check('password')
+        .isLength({min:6})
+        .withMessage('Password must be least 6 charecters long')
+];
+
