4acc99effafd8e9f04d07f33a839e04b128bd8bc
miyabisai <guirec0417@gmail.com>
2022-12-03 13:38:43 +0900

    add signin validator

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const shortId = require('shortid');
 const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 
 exports.signup=(req,res)=>{
@@ -29,9 +30,9 @@ exports.signup=(req,res)=>{
 }
 
 exports.signin=(req,res)=>{
-    const {email,password} = req.body;
+    const {mail,password} = req.body;
     //check if user exist 
-    User.findOne({email}).exec((err,user)=>{
+    User.findOne({mail}).exec((err,user)=>{
         if(err||!user){
             return res.status(400).json({
                 error:'User with that email does not exist.Please signup.'
@@ -43,8 +44,14 @@ exports.signin=(req,res)=>{
                 error:'Email and password do not mathc.'
             });
         }
+        //generate a token and send to client
+        const token = jwt.sign({_id:user._id},process.env.JWT,{expiresIn:'1d'});
+        res.cookie('token',token,{expiresIn:'1d'});
+        const {_id,username,name,email,role} = user;
+        return res.json({
+            token,user: {_id,username,name,email,role}
+        });
     })
-    //generate a token and send to client
-
+    
 }
 
