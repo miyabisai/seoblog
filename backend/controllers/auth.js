fd21fd068abea765bcdf81edcb851e0d79d1c2ee
miyabisai <guirec0417@gmail.com>
2022-12-16 02:38:30 +0900

    Add signout

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const shortId = require('shortid');
 const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+const { expressjwt} = require("express-jwt");
 
 
 exports.signup=(req,res)=>{
@@ -55,4 +55,22 @@ exports.signin=(req,res)=>{
     })
     
 }
+
+exports.signout = (req,res)=>{
+    res.clearCookie("token");
+    console.log(process.env.JWT);
+    res.json({
+        message:"Signout success."
+    });
+}
+
+const getSecret = ()=>{
+    return process.env.JWT;
+}
+
+exports.requireSignin = expressjwt({
+        secret: getSecret,
+        algorithms: ["HS256"], // added later
+        userProperty: "auth",
+});
 
